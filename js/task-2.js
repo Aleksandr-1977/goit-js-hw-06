28ab54dc1f991fbde545114f859f8c58996b6c6b
Alexander <vasilenkobox@gmail.com>
2024-12-05 14:15:37 +0200

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -7,7 +7,8 @@
 class Storage {
   #items;
   constructor(items) {
-  this.#items = [items];}
+  this.#items = items;
+  }
   getItems(){return this.#items;}
   addItem(newItem){return this.#items=newItem.push(this.#items);
 }
